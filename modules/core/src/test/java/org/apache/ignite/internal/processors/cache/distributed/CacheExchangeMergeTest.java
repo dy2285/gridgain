d5b60c02a13d6dd3221526ea73d2426f12364e5e
amashenkov <andrey.mashenkov@gmail.com>
2023-01-13 16:36:07 +0300

    Fix test exection pool size

--- modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/CacheExchangeMergeTest.java
+++ modules/core/src/test/java/org/apache/ignite/internal/processors/cache/distributed/CacheExchangeMergeTest.java
@@ -187,8 +187,9 @@ public class CacheExchangeMergeTest extends GridCommonAbstractTest {
     @Override protected void beforeTestsStarted() throws Exception {
         super.beforeTestsStarted();
 
-        executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors(),
-            new IgniteThreadFactory("testscope", "cache-exchange-metge-tests"));
+        executor = Executors.newFixedThreadPool(
+            Math.min(Runtime.getRuntime().availableProcessors(), cacheNames.length),
+            new IgniteThreadFactory("testscope", "cache-exchange-merge-tests"));
     }
 
     /** {@inheritDoc} */
